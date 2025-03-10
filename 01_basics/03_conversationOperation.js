0141b41b58b810a0564cf31d8c309b298928b0ce
Rajkamal Yadav <143881829+Rajkamalout@users.noreply.github.com>
2025-03-10 09:00:21 +0000

    commit

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,71 @@
+// let score = "Raj"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// // "33" => 33
+// // "33abc" => NaN
+// //  true => 1; false => 0
+
+// let isLoggedIn = "Raj"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//   console.log(booleanIsLoggedIn);
+
+// // 1 => true; 0 => false
+// // "" => false
+// // "RAJ" => true
+
+// let number=32
+
+// let changenumber=String(number)
+// console.log(changenumber)
+// console.log(typeof changenumber)
+
+// // *********************** Operations ***********************
+
+// let value = 3
+// let negValue = -value
+// let multiplie= value *2;
+//     console.log(value)
+//     console.log(negValue);
+//     console.log(multiplie)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+// // console.log(str3);
+
+// // console.log("1" + 2);
+// // console.log(1 + "2");
+// // console.log("1" + 2 + 2);
+// // console.log(1 + 2 + "2");
+
+// // console.log( (3 + 4) * 5 % 3);
+
+// // console.log(+true);
+// // console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// // link to study
+// // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
