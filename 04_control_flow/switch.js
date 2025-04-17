c607e2dd02614ad7aadfc72f2561e483d7946a27
Rajkamal Yadav <143881829+Rajkamalout@users.noreply.github.com>
2025-04-17 05:54:06 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month='march'
+
+switch (month){
+    case 'jan':
+        console.log('jaunary');
+        break;
+    case 'feb':
+        console.log('febuary');
+        break;
+    case 'march':
+        console.log('march');
+        break;
+    case 'april':
+        console.log("april")
+        break;
+}
